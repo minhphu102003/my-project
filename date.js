c2980004948f9eb255ba45fba7231ce975596d41
minhphu102003 <minhphu01200@gmail.com>
2023-07-11 23:23:49 +0700

    second version

---
+++ date.js
@@ -0,0 +1,7 @@
+const d = new Date();
+console.log(d);
+const date = new Date("2015");
+console.log(date);
+
+const date1 = new Date("2015-03-25T12:00:00Z");
+console.log(date1);
--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+window.addEventListener("load",function(){
+    const menuLinks = [...document.querySelectorAll('.navigation-item-link')];
+    menuLinks.forEach(item=> item.addEventListener("mouseenter",handleFunction));
+    const lineBottom = document.createElement("div");
+    lineBottom.className = "line-fixed";
+    document.body.appendChild(lineBottom);
+
+    function handleFunction(event){
+        const {left , top , height ,width} = event.target.getBoundingClientRect();
+        const marginBottom = 5;
+        const paddingRight = 5;
+        lineBottom.style.left = `${left}px`;
+        lineBottom.style.width = `${width + paddingRight}px`;
+        lineBottom.style.top = `${top+ height+marginBottom }px`;
+    }
+
+    const menu = document.querySelector('.navigation-item');
+    menu.addEventListener("mouseleave",function(){
+        lineBottom.style.width = 0;
+    })
+});
+
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
 
@@ -22,13 +44,25 @@ var sliderNav = function(manual){
 
     btns[manual].classList.add("active");
     slides[manual].classList.add("active");
-
     contents[manual].classList.add("active");
 }
+
+//!thêm sự kiện vào nút tới và nút lùi
+
+// !Thiếu sử lý set timeout cho video
+// !Thiếu sử lý mở muted cho những video đang active
+
+
+
+
+
+
 
 btns.forEach((btn, i) =>{
     btn.addEventListener("click",()=>{
         sliderNav(i);
     });
 });
+
+
 
